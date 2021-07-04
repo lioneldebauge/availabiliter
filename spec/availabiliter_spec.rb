aa3ef5de15962bba997add1f00a98b9c7443e3e2
lioneldebauge <lionel@livecolonies.com>
2021-07-04 16:39:09 +0200

    test commit

--- spec/availabiliter_spec.rb
+++ spec/availabiliter_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Availabiliter do
   describe "#availabilities" do
     subject(:subject) { described_class.get_availabilities(range_array, timeframe_start) }
 
-    let(:range_array) { [range1, range2, range3] }
+    let(:range_array) { [range2, range3, range1] }
     let(:range1) { Date.new(1999, 1, 2)..Date.new(1999, 5, 1) }
     let(:range2) { Date.new(2000, 1, 1)..Date.new(2000, 2, 1) }
     let(:range3) { Date.new(2000, 3, 1)..nil }
